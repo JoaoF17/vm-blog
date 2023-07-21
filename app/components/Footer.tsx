8a17a886e71881563f1106406cdf716dc946aee5
JoaoF17 <joao_fernandess17@hotmail.com>
2023-07-21 16:41:03 +0700

    Added main page link and instagram to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link"
+import { FaInstagram } from "react-icons/fa"
+
 export default function Footer() {
   return (
     <footer>
-      <div className="border-t border-slate-500 text-slate-400 mt-6 py-6 text-center">
-        <h3>By Vegan Monkey</h3>
+      <div className="flex border-t border-slate-500 mt-6 py-6 text-xl justify-center align-middle gap-32">
+        <div className="flex text-slate-400">
+          <h3>By &nbsp;</h3>
+          <Link href="https://veganmonkey.co/" target="_blank" rel="noopener noreferrer" className="text-slate-400 hover:text-green-700">Vegan Monkey</Link>
+        </div>
+        <div> 
+          <Link href="https://www.instagram.com/veganmonkey.co/" target="_blank" rel="noopener noreferrer" className="text-black/30 hover:text-red-400 text-3xl">
+            <FaInstagram />
+          </Link>
+        </div>
       </div>
+      
     </footer>
   )
 }
