6e31d0e51e23dd8a26a9955bf67bdb9ee8da6bc9
JoaoF17 <joao_fernandess17@hotmail.com>
2023-07-21 17:50:01 +0700

    footer css fix for small screens

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FaInstagram } from "react-icons/fa"
 export default function Footer() {
   return (
     <footer>
-      <div className="flex justify-end space-x-56 border-t border-slate-500 mt-6 py-6 text-xl">
+      <div className="flex justify-end space-x-36 md:space-x-64 border-t border-slate-500 mt-6 py-6 text-xl">
         <div className="flex justify-center text-slate-400">
           <h3>By &nbsp;</h3>
           <Link href="https://veganmonkey.co/" target="_blank" rel="noopener noreferrer" className="text-slate-400 hover:text-green-700">Vegan Monkey</Link>
