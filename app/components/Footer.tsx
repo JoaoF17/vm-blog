a28c5ebf6a77bbb8b2865f2fe2256c6012ec0a39
JoaoF17 <joao_fernandess17@hotmail.com>
2023-07-21 17:00:06 +0700

    footer css update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,12 +4,12 @@ import { FaInstagram } from "react-icons/fa"
 export default function Footer() {
   return (
     <footer>
-      <div className="flex border-t border-slate-500 mt-6 py-6 text-xl justify-center align-middle gap-32">
-        <div className="flex text-slate-400">
+      <div className="flex justify-end space-x-56 border-t border-slate-500 mt-6 py-6 text-xl">
+        <div className="flex justify-center text-slate-400">
           <h3>By &nbsp;</h3>
           <Link href="https://veganmonkey.co/" target="_blank" rel="noopener noreferrer" className="text-slate-400 hover:text-green-700">Vegan Monkey</Link>
         </div>
-        <div> 
+        <div className="flex"> 
           <Link href="https://www.instagram.com/veganmonkey.co/" target="_blank" rel="noopener noreferrer" className="text-black/30 hover:text-red-400 text-3xl">
             <FaInstagram />
           </Link>
@@ -19,3 +19,5 @@ export default function Footer() {
     </footer>
   )
 }
+
+// justify-center align-middle gap-32
